adf7375d44eec9c5c71cb8eb658ab6da0f750c4d
vinh <thevinh01102@gmail.com>
2023-12-09 23:19:17 +0700

    update

--- IOT/assets/js/main.js
+++ IOT/assets/js/main.js
@@ -1,13 +1,125 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document)
-let body = $('body')
 let themeBtn = $('.icon-theme')
-let btnDevice = $$('.btn-off')
 let lightBtn = $('.section__body__device-light-btn')
 let fanBtn = $('.section__body__device-fan-btn')
 let tvBtn = $('.section__body__device-tv-btn')
 let acBtn = $('section__body__device-ac-btn')
-let devices = ['LIGHT','FAN','TV','AC'];
+let DFdevices = ['LIGHT','FAN','TV','AC'];
+let bodyDfDevices = $('.section__body__device')
+let btnDevice = $$('.btn-off')
+let body = $('body')
+let controlbtn = $$('.otherDevice_list_item')
+let btnAddDevices = $$('.btn_add_device')
+let btnRemoveDevices = $$('.btn_remove_device')
+let otherDevices = $$('.otherDevice_cart')
+let bodyOtherDevice = $('.otherDevice__mix')
+var otherDevices_data =[
+  {
+    "id": 1,
+    "name": "radio",
+    "icon": "fa-solid fa-radio",
+    "position": "floor1"
+  },
+  {
+    "name": "light",
+    "icon": "fa-regular fa-lightbulb",
+    "position": "floor2",
+    "id": 2
+  },
+  {
+    "name": "robot",
+    "icon": "fa-solid fa-robot",
+    "position": "floor3",
+    "id": 3
+  },
+  {
+    "name": "bath",
+    "icon": "fa-solid fa-bath",
+    "position": "floor3",
+    "id": 4
+  },
+  {
+    "name": "sink",
+    "icon": "fa-solid fa-sink",
+    "position": "floor2",
+    "id": 5
+  },
+  {
+    "name": "camera",
+    "icon": "fa-solid fa-video",
+    "position": "floor1",
+    "id": 6
+  },
+  {
+    "name": "camera",
+    "icon": "fa-solid fa-video",
+    "position": "floor1",
+    "id": 7
+  },
+  {
+    "name": "wifi",
+    "icon": "fa-solid fa-wifi",
+    "position": "floor1",
+    "id": 8
+  },
+  {
+    "name": "ghost",
+    "icon": "fa-solid fa-ghost",
+    "position": "floor3",
+    "id": 9
+  }
+]
+  var brr =[]
+const df_devicesApI =' http://localhost:3000/defaultDevices'
+const other_devicesApi = 'http://localhost:3000/otherDevices'
+function render() {
+    
+    fetch(df_devicesApI)
+    .then(response => response.json())
+    .then(df_devices => {
+
+        var htmls = df_devices.map((device,index) => {
+          brr = [...brr,...[device]]
+          
+            return `<div class="section__body__device_item">
+            <div class="section__body__device-container">
+              <div class="section__body__device-icon"><i class="${device.icon}"></i></div>
+              <div class="section__body__device-title">${device.name}</div>
+            </div>
+            <div onclick="on_offDevice(event,${index})" class="section__body__device-btn btn-off "><i class="fa fa-toggle-off" aria-hidden="true"></i></div>
+
+          </div>`
+        })
+        
+        var aaa = JSON.stringify(df_devices)
+        console.log(aaa)
+        console.log(JSON.parse(aaa))
+        htmls = htmls.join('')
+        bodyDfDevices.innerHTML = htmls
+      
+    })
+
+    fetch(other_devicesApi)
+    .then(response => response.json())
+    .then(otherDevices => {
+        var htmls = otherDevices.map((device,index) => {
+          return `
+          <div href="" style="order:${device.id};"class="otherDevice_cart  mix ${device.position}">
+          <div class="otherDevice_item">
+            <div class="otherDevice_item_icon"><i class="${device.icon}"></i></div>
+            <div class="otherDevice_item_title">${device.name}</div>
+          </div>
+          <div class="otherDevice_item_place">${device.position}</div>
+          <div class="btn_add_device"><i class="fa-solid fa-plus"></i></div>
+          
+        </div>
+          `
+      })
+    })
+
+}
+
 
 function toast({ title = "", message = "", type = "info", duration = 3000 }) {
     const main = document.getElementById("toast");
@@ -55,34 +167,52 @@ function toast({ title = "", message = "", type = "info", duration = 3000 }) {
     }
   }
   
-function on_offDevice() {
-
-    Array.from(btnDevice).forEach((el,index,arr) => {
-        el.addEventListener('click',()=>{
-         
-        if(el.children[0].matches('.fa.fa-toggle-off')) {
-            el.children[0].classList.remove('fa-toggle-off')
-            el.children[0].classList.add('fa-toggle-on')
-            toast({
-                title: `${devices[index]}`,
-                message: "Is On",
-                type: "success",
-                duration: 3000
-              });
-        }
-        else if(el.children[0].matches('.fa.fa-toggle-on')) {
-            el.children[0].classList.remove('fa-toggle-on')
-            el.children[0].classList.add('fa-toggle-off')
-            toast({
-                title: `${devices[index]}`,
-                message: "Is off",
-                type: "error",
-                duration: 3000
-              });
-        }
-        })
-        
-    })
+function on_offDevice(event,index, deviceString) {
+  // var devicess = JSON.parse(...deviceString)
+    if(event.target.matches('.fa.fa-toggle-off')) {
+        event.target.classList.remove('fa-toggle-off')
+        event.target.classList.add('fa-toggle-on')
+        toast({
+            title: `${DFdevices[index]}`,
+            message: "Is On",
+            type: "success",
+            duration: 3000
+          });
+    }
+    else if(event.target.matches('.fa.fa-toggle-on')) {
+        event.target.classList.remove('fa-toggle-on')
+        event.target.classList.add('fa-toggle-off')
+        toast({
+            title: `${DFdevices[index]}`,
+            message: "Is off",
+            type: "error",
+            duration: 3000
+          });
+    }
+    
+}
+function on_offDevice2(event,index, deviceString) {
+  // var devicess = JSON.parse(...deviceString)
+    if(event.target.matches('.fa.fa-toggle-off')) {
+        event.target.classList.remove('fa-toggle-off')
+        event.target.classList.add('fa-toggle-on')
+        toast({
+            title: `${otherDevices_data[index].name}`,
+            message: "Is On",
+            type: "success",
+            duration: 3000
+          });
+    }
+    else if(event.target.matches('.fa.fa-toggle-on')) {
+        event.target.classList.remove('fa-toggle-on')
+        event.target.classList.add('fa-toggle-off')
+        toast({
+            title: `${otherDevices_data[index].name}`,
+            message: "Is off",
+            type: "error",
+            duration: 3000
+          });
+    }
     
 }
 function changeTheme() {
@@ -102,13 +232,76 @@ function changeTheme() {
         }
     })
 }
+function controlBtnActive() {
+  controlbtn.forEach((elemen, index) => {
+    elemen.addEventListener('click',() =>{
+      document.querySelector('.otherDevice_list_item.active-item').classList.remove('active-item')
+      elemen.classList.add('active-item')
+    })
+    
+  });
+}
+
+function add_removeDevice() {
+  btnAddDevices.forEach((el,index) => {
+    el.addEventListener('click',() =>{
+      otherDevices[index].remove()
+      var html = document.createElement('div')
+      html.classList.add('section__body__device_item','section__body__device_itemSelected')
+      html.setAttribute("data-index",index)
+      html.innerHTML = `
+      <div class="section__body__device-container">
+        <div class="section__body__device-icon"><i class="${otherDevices_data[index].icon}"></i></div>
+        <div class="section__body__device-title">${otherDevices_data[index].name}</div>
+      </div>
+      <div onclick="on_offDevice2(event,${index})" class="section__body__device-btn btn-off "><i class="fa fa-toggle-off" aria-hidden="true"></i></div>
+      <div onclick="removeItem(${index})" class="btn_remove_device"><i class="fa-solid fa-minus"></i></div>   
+`
+      bodyDfDevices.appendChild(html)
+    })
+    
+  })
+  // btnRemoveDevices.forEach((el,index) => {
+  //   el.addEventListener('click',() =>{
+  //     var itemSelected = $$('.section__body__device_itemSelected')
+  //     console.log(itemSelected)
+  //     itemSelected[index].remove
+  //     btnAddDevices[index].style.display = 'block'
+  //     bodyOtherDevice.appendChild(otherDevices[index])
+  //   })
+    
+  // })
+}
+function removeItem(index) {
+  var itemSelected = $$('.section__body__device_itemSelected')
+  itemSelected.forEach(el => {
+    var dataIndex = el.getAttribute("data-index")
+    if(dataIndex ==index) {
+      el.remove()
+    }
+  })
+  bodyOtherDevice.appendChild(otherDevices[index])
+}
 
 function handleClicks() {
-    on_offDevice()
-    changeTheme()
+  controlBtnActive()
+  changeTheme()
+  add_removeDevice()
 }
 function start() {
+    render()
+    
     handleClicks()
 }
 start()
 // Toast function
+let mixerDevice = mixitup('.otherDevice__mix', {
+  selectors: {
+      target: '.otherDevice_cart'
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+
--- IOT/assets/js/weather.js
+++ IOT/assets/js/weather.js
@@ -1,4 +1,3 @@
-
 const container_weather = $('.container_weather')
 const search = $('.search-box button')
 const input = $('.search-box input')
@@ -8,6 +7,7 @@ const weather_detail = $('.weather_detail')
 const temp_number = $('.section__body__environment-tem-number')
 const humi_number = $('.section__body__environment-humi-number')
 const city_title = $('.city_title')
+const btn_up = $('.btn-up')
 
 function weather() {
     const APIkey = '63f85da8c580dd6925a6548fbde84cc5'
@@ -17,24 +17,26 @@ function weather() {
 
     city_title.innerText = `${city}`
     city_title.classList.add('active')
-    
+    btn_up.classList.add('active')
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric
     &appid=${APIkey}`).then(response => response.json()).then(json => {
         
         if(json.cod == '404') {
+            temp_number.innerText = `0°C`
+            humi_number.innerText = `0%`
             container_weather.style.height = '400px'
             weather_box.classList.remove('active')
             weather_detail.classList.remove('active')
             not_found.classList.add('active')
-            console.log(not_found)
             return;
-        } 
-         
+        } else {
+       
+
         container_weather.style.height = '555px'
         weather_box.classList.add('active')
         weather_detail.classList.add('active')
         not_found.classList.remove('active')
-        
        
         const image = $('.weather img')
         const temporature = $('.weather .temporature ')
@@ -79,16 +81,28 @@ function weather() {
                 image.src = './image/cloud.png'
                 break;
         }
-        console.log(typeof(json.main.temp))
         temporature.innerHTML = `${parseInt(json.main.temp) - 273}<span>°C</span>`
         temp_number.innerText = `${parseInt(json.main.temp) - 273}°C`
         description.innerHTML = `${json.weather[0].description}`
         humidity.innerHTML = `${json.main.humidity}%`
         humi_number.innerText = `${json.main.humidity}%`
         wind.innerHTML = `${parseInt(json.wind.speed)}km/h`
+        }
+       
     })
 }
+function btn_up_Clicked() {
+    weather_box.classList.remove('active')
+    weather_detail.classList.remove('active')
+    not_found.classList.remove('active')
+    city_title.classList.remove('active')
+    container_weather.style.height = '91px'
+    btn_up.classList.remove('active')
+
+}
+
 search.addEventListener('click', weather)
+btn_up.addEventListener('click',btn_up_Clicked)
 input.addEventListener('keypress', (e) => {
     if(e.key == 'Enter') {
         weather()
